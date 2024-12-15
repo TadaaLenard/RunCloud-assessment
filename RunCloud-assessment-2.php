0db2865c90658eff35fa894044fd5a603b83c2df
TadaaLenard <148462930+TadaaLenard@users.noreply.github.com>
2024-12-15 14:48:57 +0800

    Delete RunCloud-assessment-2.php

--- RunCloud-assessment-2.php
+++
@@ -1,5 +0,0 @@
-<php>
-echo "<h2>Name: Lim Shou Zhuin</h2><div>";
-echo "<p>I am new to PHP language.</p><div>";
-echo "<p>This is just a simple website with some texts, wish I will explore more about coding PHP.</p>";
-</php>
