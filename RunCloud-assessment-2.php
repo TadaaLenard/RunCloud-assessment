501f83bd5b221ef768fefc61f0321af20fa61243
TadaaLenard <lenard8794@gmail.com>
2024-12-15 14:51:05 +0800

    new

---
+++ RunCloud-assessment-2.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h2>Name: Lim Shou Zhuin</h2>";
+echo "<div>";
+echo "<p>I am new to PHP language.</p>";
+echo "<p>This is just a simple website with some texts, wish I will explore more about coding PHP.</p>";
+echo "</div>";
+?>
